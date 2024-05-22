e365d9d910e91b1369a39f31ed91fd8f72c90f8f
Faishal Fernando Hutama <lonnerid71@gmail.com>
2024-05-22 21:29:53 +0700

    resolve error

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import AuthProvider from 'react-auth-kit/AuthProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
